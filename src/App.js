005483e8f89359a93c33efc231b154cadf3ad482
Dudu <eduardoromeueberts@gmail.com>
2024-09-22 20:18:30 -0300

    Corrigido forms duplicado

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ function App() {
       <div className="App">
         <Header titulo="MIAUDOT"></Header>
         <Cadastro />
-        <FormPropsTextFields />
       </div>
     </ThemeProvider>
   );
