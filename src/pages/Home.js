8691e55b4f3d6d99b1a5650a085a8a2054a50ada
Dudu <eduardoromeueberts@gmail.com>
2024-09-30 21:36:11 -0300

    Adc PetCard e modal de cadastro

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
-import { Container, Grid, Card, CardContent, Typography } from '@mui/material';
+import { Container, Grid2, Card, CardContent, Typography } from '@mui/material';
+import PetCard from '../components/PetCard/PetCard';
 //Container: Centraliza o conteúdo.
 //Grid: Organiza os cards dos pets de forma responsiva.
 //Card: Cada pet para adoção é representado por um card.
@@ -10,8 +10,9 @@ import { Container, Grid, Card, CardContent, Typography } from '@mui/material';
 function Home() {
   // Exemplo de dados
   const pets = [
-    { id: 1, name: 'Fofinho', description: 'Gato muito carinhoso!' },
-    { id: 2, name: 'Rex', description: 'Cachorro brincalhão e esperto.' },
+    { id: 1, name: 'Fofinho', description: 'Gato muito carinhoso!', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKy5Zq3nDNcIKQEtTvd1iJTSzxQk4UO53QrA&s' },
+    { id: 2, name: 'Rex', description: 'Cachorro brincalhão e esperto.', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTekrqEm8Pps8NR1x2kRA2N2WTL23Q9R9nVbw&s' },
+    { id: 3, name: 'Rufus', description: 'Cachorro dócil e amigável.' }
   ];
 
   return (
@@ -19,18 +20,19 @@ function Home() {
       <Typography variant="h4" component="h1" gutterBottom>
         Pets para Adoção
       </Typography>
-      <Grid container spacing={3}>
-        {pets.map((pet) => (
-          <Grid item xs={12} sm={6} md={4} key={pet.id}>
-            <Card>
-              <CardContent>
-                <Typography variant="h5">{pet.name}</Typography>
-                <Typography>{pet.description}</Typography>
-              </CardContent>
-            </Card>
-          </Grid>
+      <Grid2 container spacing={3}>
+        {pets.map((pet, index) => (
+          <PetCard name={pet.name} description={pet.description} image={pet.image} key={pet.id} />
+          // <Grid2 item xs={12} sm={6} md={4} key={pet.id}>
+          //   <Card>
+          //     <CardContent>
+          //       <Typography variant="h5">{pet.name}</Typography>
+          //       <Typography>{pet.description}</Typography>
+          //     </CardContent>
+          //   </Card>
+          // </Grid2>
         ))}
-      </Grid>
+      </Grid2>
     </Container>
   );
 }
