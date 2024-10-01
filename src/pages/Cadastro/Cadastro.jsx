0e36089345ac34fcf02dcc6da817cabaa24cdf84
stEElpuLse <100028301+stEElpuLs3@users.noreply.github.com>
2024-10-01 20:44:27 -0300

    novo botao pagina de cadastro

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -4,13 +4,32 @@ import ColorButtons from "../../components/Button/button";
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import { Stack } from "@mui/material";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 export default function Cadastro() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     return (
         <Fragment>
             <h1>CADASTRO DE USUÁRIO</h1>
-            <Stack 
+            <Stack
                 component="form"
                 sx={{ display: "flex", alignItems: "center", '& .MuiTextField-root': { m: 1, width: '25ch' } }}
                 noValidate
@@ -42,9 +61,26 @@ export default function Cadastro() {
                     label="Email"
                     type="mail"
                 />
-                <ColorButtons />
 
             </Stack>
+            <div>
+                <Button onClick={handleOpen}>Confirmar</Button>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Parabens!
+                        </Typography>
+                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                            Seu cadastro foi realizado com sucesso!
+                        </Typography>
+                    </Box>
+                </Modal>
+            </div>
         </Fragment>
     );
 }
