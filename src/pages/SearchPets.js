d3c3febd741ce64e246f4cd4371042330b98fa77
stEElpuLse <100028301+stEElpuLs3@users.noreply.github.com>
2024-10-01 21:16:20 -0300

    pagina searchpets configurada

--- src/pages/SearchPets.js
+++ src/pages/SearchPets.js
@@ -1,13 +1,62 @@
 import React from 'react';
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, Box, Slider, Button, Stack, } from '@mui/material';
+
+const MAX = 100;
+const MIN = 1;
+const marks = [
+  {
+    value: MIN,
+    label: 'KM',
+  },
+  {
+    value: MAX,
+    label: '',
+  },
+];
 
 function SearchPets() {
+  const [val, setVal] = React.useState(MIN);
+  const handleChange = (_, newValue) => {
+    setVal(newValue);
+  };
+
   return (
-    <Container>
+    <Container sx={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
       <Typography variant="h4" component="h1" gutterBottom>
         Buscar Pets por Região
       </Typography>
-      {/* Implementação de geolocalização e busca */}
+      <Box sx={{ width: 350 }}>
+        <Slider
+          marks={marks}
+          step={10}
+          value={val}
+          valueLabelDisplay="auto"
+          min={MIN}
+          max={MAX}
+          onChange={handleChange}
+        />
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography
+            variant="body2"
+            onClick={() => setVal(MIN)}
+            sx={{ cursor: 'pointer' }}
+          >
+            {MIN}
+          </Typography>
+          <Typography
+            variant="body2"
+            onClick={() => setVal(MAX)}
+            sx={{ cursor: 'pointer' }}
+          >
+            {MAX}
+          </Typography>
+        </Box>
+      </Box>
+      <Stack direction="row" spacing={2}>
+        <Button variant="contained" color="success">
+          Confirmar
+        </Button>
+      </Stack>
     </Container>
   );
 }
