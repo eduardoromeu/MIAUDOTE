bd498633da1c61ff8e359f656dfb0523fd2790ad
Dudu <eduardoromeueberts@gmail.com>
2025-09-13 21:40:13 -0300

    Update SearchPets.js

--- src/pages/SearchPets.js
+++ src/pages/SearchPets.js
@@ -19,27 +19,12 @@ const bbox_offset = 10;
 
 function SearchPets() {
   const [val, setVal] = React.useState(MIN); // Estado para armazenar o valor do slider
-  const [geoData, setGeoData] = React.useState(null);
-  
+
   // Função para atualizar o valor do slider
   const handleChange = (_, newValue) => {
     setVal(newValue); // Atualiza o estado com o novo valor do slider
   };
   
-  React.useEffect(() => {
-    const url = `https://api.ipgeolocation.io/v2/ipgeo?apiKey=${geoip_key}`; 
-
-    fetch(url)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Erro na requisição");
-        }
-        return res.json();
-      })
-      .then((json) => {setGeoData(json); console.log(geoData)})
-      .catch((err) => console.error("Erro:", err));
-  }, []);
-
   return (
     <Container
       sx={{
@@ -89,11 +74,7 @@ function SearchPets() {
 
       <Box
           component="iframe"
-          // src={`https://www.openstreetmap.org/export/embed.html?bbox=-52.23396%2C-24.89196%2C-52.19632%2C-24.87882&amp;layer=mapnik`}
-          src={
-            geoData && geoData.latitude ? `https://www.openstreetmap.org/export/embed.html?bbox=-${geoData.latitude}%2C${geoData.longitude}%2C${geoData.latitude+0.2}%2C${geoData.longitude+0.2}&amp;layer=mapnik`
-            : `https://www.openstreetmap.org/export/embed.html?bbox=-52.23396%2C-24.89196%2C-52.19632%2C-24.87882&amp;layer=mapnik`
-          }
+          src={`https://www.openstreetmap.org/export/embed.html?bbox=-52.23396%2C-24.89196%2C-52.19632%2C-24.87882&amp;layer=mapnik`}
           alt="Mapa"
           loading="lazy"
           sx={{
